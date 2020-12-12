469964b23fc67ae9a89eded035854d4b720bea92
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2020-12-12 12:31:55 +0300

    feat: complete regex/*

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  const reg = /{[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}/i;
+  return reg;
 }
 
 
@@ -54,7 +55,8 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  const reg = /p[a-z\s]t/;
+  return reg;
 }
 
 
@@ -78,8 +80,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const reg = new RegExp(`(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=\\S+$)[0-9a-zA-Z]{${minLength},}`);
+  return reg;
 }
 
 
